78aa63e3888f43c4dcd8e8e9160d14d68e441c37
rahul4019 <rahulkashyap4019@gmail.com>
2024-01-11 08:04:51 +0530

    Add function to append and resize the array in the mylist  class

--- dynamic_array.py
+++ dynamic_array.py
@@ -9,9 +9,33 @@ class MyList:
         # create a C type array with size = self.size
         self.A = self.__make_array(self.size)
 
-    # returns lenght
+    # returns length
     def __len__(self):
         return self.n
+
+    # increases the capacity of the array and copy-paste the items of first array to the new array
+    def __resize(self, new_capacity):
+        # create a new array with new capacity
+        self.B = self.__make_array(new_capacity)
+        self.size = new_capacity
+
+        # copy the content of A to B
+        for i in range(self.n):
+            self.B[i] = self.A[i]
+
+        # reassign A
+        self.A = self.B
+
+    # inserts the item in the array
+    def append(self, item):
+        # first check if there is vacant space in the array
+        if self.n == self.size:
+            # increase the size of the array
+            self.__resize(self.size*2)
+
+        # append the item in the array
+        self.A[self.n] = item
+        self.n += 1
 
     def __make_array(self, capacity):
         # creates a C type array(static, referential) with size capacity
@@ -19,7 +43,13 @@ class MyList:
 
 
 L = MyList()
-print(type(L))
-print(L)  # will pring memory address of L
+# print(type(L))
+# print(L)  # will pring memory address of L
+
+
+L.append('Hello')
+L.append(3.4)
+L.append(True)
+L.append(100)
 
 print(len(L))
