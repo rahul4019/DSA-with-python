3c909da81d01a2f81ea7e0a46ff841037fb5834b
rahul4019 <rahulkashyap4019@gmail.com>
2024-01-18 14:38:54 +0530

    Add insert_head method in LinkedList

--- linked_list.py
+++ linked_list.py
@@ -11,8 +11,25 @@ class Linkedlist:
     
     def __len__(self):
         return self.n
+    
+    def insert_head(self,value):
+        # create a new node
+        new_node = Node(value)
+
+        # create connection
+        new_node.next = self.head
+
+        # reassign head
+        self.head = new_node
+
+        # increment n
+        self.n += 1
 
 L = Linkedlist() 
+L.insert_head(1)
+L.insert_head(2)
+L.insert_head(3)
+L.insert_head(4)
 print(len(L))   
 
  
