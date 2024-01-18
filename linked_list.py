8633dbfbc6a541811c11cdaaa981da412c92cd90
rahul4019 <rahulkashyap4019@gmail.com>
2024-01-18 14:49:30 +0530

    Implement __str__ method which prints the Linked List

--- linked_list.py
+++ linked_list.py
@@ -3,16 +3,17 @@ class Node:
         self.data = value
         self.next = None
 
+
 class Linkedlist:
     def __init__(self):
         # create a empty linked list
         self.head = None
         self.n = 0
-    
+
     def __len__(self):
         return self.n
-    
-    def insert_head(self,value):
+
+    def insert_head(self, value):
         # create a new node
         new_node = Node(value)
 
@@ -25,11 +26,22 @@ class Linkedlist:
         # increment n
         self.n += 1
 
-L = Linkedlist() 
+    def __str__(self):
+        curr = self.head
+        result = ''
+
+        while curr != None:
+            result += str(curr.data) + '->'
+            curr = curr.next
+
+        return result[:-2]
+
+
+
+L = Linkedlist()
 L.insert_head(1)
 L.insert_head(2)
 L.insert_head(3)
 L.insert_head(4)
-print(len(L))   
-
- 
+print(len(L))
+print(L)
