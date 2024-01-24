3dc5fbbb6bd0d239e062e5adabff4d621351c969
rahul4019 <rahulkashyap4019@gmail.com>
2024-01-24 19:44:09 +0530

    Implement clear, delete_head, delete_head, pop and remove method to LL

--- linked_list.py
+++ linked_list.py
@@ -10,9 +10,11 @@ class Linkedlist:
         self.head = None
         self.n = 0
 
+    # returns length of lined list
     def __len__(self):
         return self.n
 
+    # inserts the value at 1st postion
     def insert_head(self, value):
         # create a new node
         new_node = Node(value)
@@ -26,6 +28,7 @@ class Linkedlist:
         # increment n
         self.n += 1
 
+    # prints the LL
     def __str__(self):
         curr = self.head
         result = ''
@@ -36,6 +39,7 @@ class Linkedlist:
 
         return result[:-2]
 
+    # adds item at the end of LL
     def append(self, value):
         new_node = Node(value)
 
@@ -53,6 +57,7 @@ class Linkedlist:
         curr.next = new_node
         self.n += 1
 
+    # inserts item next to given item
     def insert_after(self, after, value):
         new_node = Node(value)
 
@@ -67,10 +72,70 @@ class Linkedlist:
         if curr != None:
             new_node.next = curr.next
             curr.next = new_node
+            self.n += 1
         # case 2 -> item not found
         else:
             print('Item not found')
-            return 
+            return
+
+    # makes the LL empty
+    def clear(self):
+        self.head = None
+        self.n = 0
+
+    # deltes head of the LL
+    def delete_head(self):
+        # in case linked list is empty
+        if self.head == None:
+            return
+        self.head = self.head.next
+        self.n -= 1
+
+    # removes the last item of the LL
+    def pop(self):
+        # case -> empty linked list
+        if self.head == None:
+            return
+
+        # case -> single item in the linked list
+        if self.head.next == None:
+            self.delete_head()
+            return
+
+        curr = self.head
+        while curr.next.next != None:
+            curr = curr.next
+
+        # curr is the 2nd last item
+        curr.next = None
+        self.n -= 1
+
+    # removes the given item from the LL
+    def remove(self, value):
+        # case1 -> empty linked list
+        if self.head == None:
+            return
+
+        prev = None
+        curr = self.head
+
+        # case2 -> value is in head node
+        if prev == None and curr.data == value:
+            self.delete_head()
+            self.n -= 1
+            return
+
+        while curr.next != None:
+            if curr.data == value:
+                break
+            prev = curr
+            curr = curr.next
+        # case 3 -> item not found
+        if curr.next == None:
+            return 'Item not found'
+        else:
+            prev.next = curr.next
+            self.n -= 1
 
 
 L = Linkedlist()
@@ -78,7 +143,12 @@ L.insert_head(1)
 L.insert_head(2)
 L.insert_head(3)
 L.insert_head(4)
-print(len(L))
-# L.append(5)
-L.insert_after(1, 200)
+# print(len(L))
+L.append(5)
+# L.insert_after(1, 200)
+print(L)
+# L.delete_head()
+# L.pop()
+L.remove(25)
+# print(f'after deletion {L}')
 print(L)
