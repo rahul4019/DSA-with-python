ae8f794ea59f218b70c4cb8570831355e7cface7
rahul4019 <rahulkashyap4019@gmail.com>
2024-11-21 17:41:31 +0530

    Fix base case condition of binary research

--- binary_search.py
+++ binary_search.py
@@ -1,21 +1,21 @@
-def binary_search(list, start, size, target):
+def binary_search(list, low, high, target):
     # base case
-    if size <= 0:
+    if low <= high:
         return -1
 
-    mid = int((start + size) / 2)
+    mid = int((low + high) / 2)
     print("mid: ", mid)
 
     if list[mid] == target:
         return mid
 
     elif list[mid] < target:
-        start = mid + 1
+        low = mid + 1
 
     elif list[mid] > target:
-        size = mid - 1
+        high = mid - 1
 
-    return binary_search(list, start, size, target)
+    return binary_search(list, low, high, target)
 
 
-print(binary_search([1, 2, 3, 4, 5], 0, 5, 5))
+print(binary_search([1, 2, 3, 4, 5], 0, 5, 200))
