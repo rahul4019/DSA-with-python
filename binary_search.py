680a8ee44be31ccb0a3f224e7ee897aa7edcd304
rahul4019 <rahulkashyap4019@gmail.com>
2024-11-21 18:05:10 +0530

    Implement binary search with while loop

--- binary_search.py
+++ binary_search.py
@@ -1,10 +1,9 @@
-def binary_search(list, low, high, target):
+def recursive_binary_search(list, low, high, target):
     # base case
     if low <= high:
         return -1
 
     mid = int((low + high) / 2)
-    print("mid: ", mid)
 
     if list[mid] == target:
         return mid
@@ -15,7 +14,28 @@ def binary_search(list, low, high, target):
     elif list[mid] > target:
         high = mid - 1
 
-    return binary_search(list, low, high, target)
+    return recursive_binary_search(list, low, high, target)
 
 
-print(binary_search([1, 2, 3, 4, 5], 0, 5, 200))
+# print(recursive_binary_search([1, 2, 3, 4, 5], 0, 5, 200))
+
+
+def binary_search(list, low, high, target):
+    while low <= high:
+        mid = (low + high) // 2
+        print("mid: ", mid)
+
+        if list[mid] == target:
+            return mid
+
+        elif list[mid] < target:
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    # not found in the list
+    return -1
+
+
+print(binary_search([1, 2, 3, 4, 5], 0, 5, 7))
