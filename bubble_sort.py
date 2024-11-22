92dcd679777b7ab671a2e5c6d3383a357c7d7c9e
rahul4019 <rahulkashyap4019@gmail.com>
2024-11-22 12:29:11 +0530

    Make bubble sort adaptive (reduce best case time complexity)

--- bubble_sort.py
+++ bubble_sort.py
@@ -10,10 +10,15 @@
 
 
 def bubble_sort(arr):
+    flag = 0
     for i in range(len(arr) - 1):
         for j in range(len(arr) - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                flag = 1
+
+        if flag == 0:
+            break
 
     return arr
 
