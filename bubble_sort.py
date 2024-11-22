202a329551122953aa869e1b231ad0ef698224ed
rahul4019 <rahulkashyap4019@gmail.com>
2024-11-22 12:16:19 +0530

    Optimize bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,14 +1,23 @@
+# def bubble_sort(arr):
+#     for i in range(len(arr) - 1):
+#         for j in range(len(arr) - i - 1):
+#             if arr[j] > arr[j + 1]:
+#                 temp = arr[j]
+#                 arr[j] = arr[j + 1]
+#                 arr[j + 1] = temp
+#
+#     return arr
+
+
 def bubble_sort(arr):
-    for i in range(len(arr)):
+    for i in range(len(arr) - 1):
         for j in range(len(arr) - i - 1):
             if arr[j] > arr[j + 1]:
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     return arr
 
 
 # print(bubble_sort([5, 4, 3, 2, 1]))
-# print(bubble_sort([5, 4, 3, 2, 1]))
-print(bubble_sort([10, 4, 9, 27, 18, 29, 30]))
+print(bubble_sort([5, 4, 3, 2, 1]))
+# print(bubble_sort([10, 4, 9, 27, 18, 29, 30]))
